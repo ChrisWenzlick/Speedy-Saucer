daf116e8291fc236c11e5c2874205e4f6a6e72a7
ChrisWenzlick <cwenzlick01@gmail.com>
2024-05-05 22:39:38 -0500

    Finish the base game
    Finish the game as demonstrated in the course. there are labels for the start, end, and tutorial text, the player will be sent back to start upon leaving the maze, and the background scales with the game window.

--- Speedy Saucer/scenes/player.gd
+++ Speedy Saucer/scenes/player.gd
@@ -1,11 +1,23 @@
 extends RigidBody2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@export var move_speed:float = 2500.0
+
+var move_direction:Vector2
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	move_direction = Vector2(0,0)
+	
+	if Input.is_action_pressed("move_left"):
+		move_direction.x -= 1.0
+	if Input.is_action_pressed("move_right"):
+		move_direction.x += 1.0
+	if Input.is_action_pressed("move_up"):
+		move_direction.y -= 1.0
+	if Input.is_action_pressed("move_down"):
+		move_direction.y += 1.0
+	
+
+func _physics_process(delta):
+	apply_force(move_direction.normalized() * move_speed)
